2e9551e9f52f5c330be803859c5b4c06902f3310
jaoguimaraes <joao.pedrogs@live.com>
2023-08-28 22:28:22 -0300

    exercício e exemplo de códigos para não seguir

---
+++ java-terminal/java-terminal/src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    /**
+    * @param numeroUm este é o primeiro parametro do método.
+    * @param numeroDois este é o segundo parametro do método.
+    * @return int o resultado deste método é a soma dos dois números.
+     */
+     public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois);
+    }
+}
